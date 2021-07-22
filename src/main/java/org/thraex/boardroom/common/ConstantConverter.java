32259fbf51018880222bd927cfa5e522a06151fb
鬼王 <THRAEX@aliyun.com>
2021-07-22 15:45:31 +0800

    feat: add whether converter

--- src/main/java/org/thraex/boardroom/common/ConstantConverter.java
+++ src/main/java/org/thraex/boardroom/common/ConstantConverter.java
@@ -3,6 +3,7 @@ package org.thraex.boardroom.common;
 import org.thraex.boardroom.common.constant.ApprovalStatus;
 import org.thraex.boardroom.common.constant.BookingType;
 import org.thraex.boardroom.common.constant.OrderStatus;
+import org.thraex.toolkit.constant.Whether;
 import org.thraex.toolkit.jpa.IntegerEnumAttributeConverter;
 
 import javax.persistence.Converter;
@@ -12,6 +13,9 @@ import javax.persistence.Converter;
  * @date 2021/07/22 13:23
  */
 public final class ConstantConverter {
+
+    @Converter(autoApply = true)
+    public static class WhetherConverter extends IntegerEnumAttributeConverter<Whether> {}
 
     @Converter(autoApply = true)
     public static class BookingTypeConverter extends IntegerEnumAttributeConverter<BookingType> {}
