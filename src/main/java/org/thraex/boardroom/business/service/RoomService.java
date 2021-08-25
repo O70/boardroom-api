d0b51b0ea8cd8a857956a97de7dad1a53fb4ce2d
鬼王 <THRAEX@aliyun.com>
2021-08-25 16:19:55 +0800

    fix: room update

--- src/main/java/org/thraex/boardroom/business/service/RoomService.java
+++ src/main/java/org/thraex/boardroom/business/service/RoomService.java
@@ -1,15 +1,15 @@
 package org.thraex.boardroom.business.service;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.thraex.boardroom.business.entity.Room;
 import org.thraex.toolkit.jpa.service.impl.GenericServiceImpl;
+import org.thraex.toolkit.util.BeanUtils;
 
 import java.util.Optional;
 
 /**
  * @author 鬼王
- * @date 2021/07/23 10:39
+ * @date 2021/07/23 101:39
  */
 @Service
 public class RoomService extends GenericServiceImpl<Room, String> {
@@ -20,8 +20,8 @@ public class RoomService extends GenericServiceImpl<Room, String> {
                 .map(id -> this.findById(id).orElse(null))
                 .orElse(null);
         Optional.ofNullable(old).ifPresent(it -> {
-            BeanUtils.copyProperties(room, old);
-            this.save(old);
+            BeanUtils.copyProperties(room, it, true);
+            this.save(it);
         });
 
         return old;
