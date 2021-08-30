563f55bf00be17f62685333dae140dc696a63652
鬼王 <THRAEX@aliyun.com>
2021-08-30 16:07:10 +0800

    opt: auditor config

---
+++ src/main/java/org/thraex/boardroom/config/AuditorAwareConfig.java
@@ -0,0 +1,22 @@
+package org.thraex.boardroom.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+/**
+ * @author 鬼王
+ * @date 2021/08/30 14:27
+ */
+@Configuration
+@EnableJpaAuditing(modifyOnCreate = false)
+public class AuditorAwareConfig implements AuditorAware<String> {
+
+    @Override
+    public Optional getCurrentAuditor() {
+        return Optional.ofNullable("HANZO");
+    }
+
+}
--- src/main/java/org/thraex/boardroom/config/ImportConfiguration.java
+++ src/main/java/org/thraex/boardroom/config/ImportConfiguration.java
@@ -1,14 +1,10 @@
 package org.thraex.boardroom.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.thraex.toolkit.aop.JpaEntityAspect;
 
 /**
  * @author 鬼王
  * @date 2021/08/26 15:46
  */
 @Configuration
-@Import(JpaEntityAspect.class)
-public class ImportConfiguration {
-}
+public class ImportConfiguration {}
