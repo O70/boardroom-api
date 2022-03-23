dfc129cee392b9448ce40a0dedb1c8c8b09d558a
鬼王 <THRAEX@aliyun.com>
2022-03-23 22:20:51 +0800

    save(auth): RestAuthenticationConverter

--- src/main/java/org/thraex/admin/auth/RestAuthenticationConverter.java
+++ src/main/java/org/thraex/admin/auth/RestAuthenticationConverter.java
@@ -1,5 +1,7 @@
 package org.thraex.admin.auth;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.server.authentication.ServerAuthenticationConverter;
@@ -11,6 +13,12 @@ import reactor.core.publisher.Mono;
  * @date 2022/03/22 18:44
  */
 public class RestAuthenticationConverter implements ServerAuthenticationConverter {
+
+    private Logger logger = LoggerFactory.getLogger(RestAuthenticationConverter.class);
+
+    public static RestAuthenticationConverter of() {
+        return new RestAuthenticationConverter();
+    }
 
     @Override
     public Mono<Authentication> convert(ServerWebExchange exchange) {
--- src/main/java/org/thraex/admin/config/SecurityConfig.java
+++ src/main/java/org/thraex/admin/config/SecurityConfig.java
@@ -2,20 +2,18 @@ package org.thraex.admin.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
-import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
-import org.springframework.security.web.server.authentication.ServerAuthenticationEntryPointFailureHandler;
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher;
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
 import org.thraex.admin.auth.JpaUserDetailsService;
+import org.thraex.admin.auth.RestAuthenticationConverter;
+import org.thraex.admin.auth.RestServerAuthenticationFailureHandler;
 import org.thraex.admin.auth.RestServerAuthenticationSuccessHandler;
 import org.thraex.admin.system.repository.UserRepository;
 
@@ -28,54 +26,36 @@ public class SecurityConfig {
 
     private ServerWebExchangeMatcher matcher = ServerWebExchangeMatchers.pathMatchers(HttpMethod.POST, "/auth/login");
 
+    //@Bean
+    //ReactiveUserDetailsService userDetailsService(UserRepository userRepository) {
+    //    return new JpaUserDetailsService(userRepository);
+    //}
+
     @Bean
-    ReactiveUserDetailsService userDetailsService(UserRepository userRepository) {
-        return new JpaUserDetailsService(userRepository);
+    ReactiveAuthenticationManager authenticationManager(UserRepository userRepository) {
+        JpaUserDetailsService userDetailsService = new JpaUserDetailsService(userRepository);
+        return new UserDetailsRepositoryReactiveAuthenticationManager(userDetailsService);
     }
 
     @Bean
-    SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http,
-                                               ReactiveUserDetailsService userDetailsService) {
-        //UserDetails user = User
-        //        .withUsername("master1")
-        //        .password("admin")
-        //        .roles("SUPER_ADMIN")
-        //        .passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder()::encode)
-        //        .build();
-
-        //MapReactiveUserDetailsService userDetailsService = new MapReactiveUserDetailsService(user);
-        //ReactiveAuthenticationManager authenticationManager = new UserDetailsRepositoryReactiveAuthenticationManager(userDetailsService());
-
-        //UsernamePasswordAuthenticationFilter upFilter = new UsernamePasswordAuthenticationFilter();
-        //upFilter.setRequiresAuthenticationRequestMatcher();
-        //upFilter.setAuth
-
-        ReactiveAuthenticationManager manager = authenticationManager(userDetailsService);
+    SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http, ReactiveAuthenticationManager manager) {
         http.authorizeExchange()
                 //.pathMatchers("/login").permitAll()
                 .anyExchange().authenticated()
                 .and()
                 .csrf().disable().headers().frameOptions().disable()
                 .and()
+                .httpBasic()
+                .and()
+                .formLogin()
+                .and()
                 .authenticationManager(manager)
-                .addFilterAt(webFilter(manager), SecurityWebFiltersOrder.AUTHENTICATION)
+                .addFilterAt(webFilter(manager), SecurityWebFiltersOrder.HTTP_BASIC)
                 //.exceptionHandling()
                 //.accessDeniedHandler(new HttpStatusServerAccessDeniedHandler(HttpStatus.UNAUTHORIZED))
-                //.httpBasic()
-                //.and()
-                //.formLogin()
-                //.and()
-
-                //.addFilterAt()
-                //.exceptionHandling().accessDeniedHandler(new HttpStatusServerAccessDeniedHandler(HttpStatus.FORBIDDEN))
-                //.securityContextRepository()
         ;
 
         return http.build();
-    }
-
-    private ReactiveAuthenticationManager authenticationManager(ReactiveUserDetailsService userDetailsService) {
-        return new UserDetailsRepositoryReactiveAuthenticationManager(userDetailsService);
     }
 
     private AuthenticationWebFilter webFilter(ReactiveAuthenticationManager manager) {
@@ -86,8 +66,10 @@ public class SecurityConfig {
         AuthenticationWebFilter filter = new AuthenticationWebFilter(manager);
         filter.setRequiresAuthenticationMatcher(matcher);
         filter.setAuthenticationSuccessHandler(RestServerAuthenticationSuccessHandler.of());
-        HttpStatusServerEntryPoint entryPoint = new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED);
-        filter.setAuthenticationFailureHandler(new ServerAuthenticationEntryPointFailureHandler(entryPoint));
+        filter.setAuthenticationFailureHandler(RestServerAuthenticationFailureHandler.of());
+        //HttpStatusServerEntryPoint entryPoint = new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED);
+        //filter.setAuthenticationFailureHandler(new ServerAuthenticationEntryPointFailureHandler(entryPoint));
+        filter.setServerAuthenticationConverter(RestAuthenticationConverter.of());
 
         return filter;
     }
